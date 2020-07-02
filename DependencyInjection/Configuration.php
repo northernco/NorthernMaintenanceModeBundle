817fd652f2245bcd3e22fc6bb97878b2cb563176
Cody deVries <cdevries@northern.co>
2020-07-02 14:45:36 -0400

    Formatting cleanup

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -11,19 +11,21 @@ class Configuration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder('northern_maintenance_mode');
 
+        // @formatter:off
         $treeBuilder->getRootNode()
                     ->children()
-                    ->scalarNode('maintenance_flag_path')
-                    ->info('This is where the maintenance flag is placed within the project folder structure.')
-                    ->defaultValue('%kernel.project_dir%/var/maintenance.flag')
-                    ->cannotBeEmpty()
-                    ->end()
-                    ->integerNode('retry_after')
-                    ->info('This is how long (in milliseconds) the user agent should wait before making a follow-up request.')
-                    ->defaultValue(300)
-                    ->min(0)
-                    ->end()
+                        ->scalarNode('maintenance_flag_path')
+                            ->info('This is where the maintenance flag is placed within the project folder structure.')
+                            ->defaultValue('%kernel.project_dir%/var/maintenance.flag')
+                            ->cannotBeEmpty()
+                        ->end()
+                        ->integerNode('retry_after')
+                            ->info('This is how long (in milliseconds) the user agent should wait before making a follow-up request.')
+                            ->defaultValue(300)
+                            ->min(0)
+                        ->end()
                     ->end();
+        // @formatter:on
 
         return $treeBuilder;
     }
